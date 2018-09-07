f7123cbd275af6136e0f70c9650c9cd6bd8ed701
kowallus <kowallus@o2.pl>
2018-09-07 10:37:41 +0200

    PgStrandDetector tool stub

---
+++ PgStrandDetector.cpp
@@ -0,0 +1,50 @@
+#include <cstdlib>
+#include <unistd.h>
+
+#include "helper.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+    int opt; // current option
+    int overlap_threshold = 40;
+
+    while ((opt = getopt(argc, argv, "k?")) != -1) {
+        switch (opt) {
+            case 'k':
+                overlap_threshold = atoi(optarg);
+                break;
+            case '?':
+                default: /* '?' */
+                fprintf(stderr, "Usage: %s [-k overlap_threshold] pgfile readssrcfile [pairsrcfile] outputprefix\n\n",
+                    argv[0]);
+                fprintf(stderr, "\n\n");
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    if (optind > (argc - 3) || optind < (argc - 4)) {
+        fprintf(stderr, "%s: Expected 3 or 4 arguments after options (found %d)\n", argv[0], argc-optind);
+        fprintf(stderr, "try '%s -?' for more information\n", argv[0]);
+        
+        exit(EXIT_FAILURE);
+    }
+
+    string pgFile(argv[optind++]);
+    string srcFile(argv[optind++]);
+    string pairFile = "";
+    if (optind == argc - 2)
+        pairFile = argv[optind++];
+    string outPrefix(argv[optind++]);
+
+    std::ifstream pgSrc(pgFile, std::ios::in | std::ios::binary);
+    if (pgSrc.fail()) {
+        fprintf(stderr, "cannot open pseudogenome file %s\n", pgFile.c_str());
+        exit(EXIT_FAILURE);
+    }
+    pgSrc.close();
+
+    exit(EXIT_SUCCESS);
+}
